d2a0d049d69114701a7afadb9bdcba1d519fff6f
Devin Jean <emailcruzjean@yahoo.com>
2024-11-11 12:56:29 -0600

    add resolver ops

--- src/ai/tf.cpp
+++ src/ai/tf.cpp
@@ -8,19 +8,20 @@ constexpr u32 tensor_arena_size = 512 * 1024;
 u8 tensor_arena[tensor_arena_size];
 
 Tensor<f32, 1> inference(const Tensor<f32, 2> &input) {
-    static const tflite::Model *model = tflite::GetModel(model_tflite);
+    const tflite::Model *model = tflite::GetModel(model_tflite);
+    if (model->version() != TFLITE_SCHEMA_VERSION) throw std::runtime_error("wrong model schema version");
 
-    if (model->version() != TFLITE_SCHEMA_VERSION) {
-        throw std::runtime_error("wrong model schema version");
-    }
+    tflite::MicroMutableOpResolver<7> resolver;
+    if (resolver.AddQuantize() != kTfLiteOk) throw std::runtime_error("failed to add quantize op to resolver");
+    if (resolver.AddReshape() != kTfLiteOk) throw std::runtime_error("failed to add reshape op to resolver");
+    if (resolver.AddConv2D() != kTfLiteOk) throw std::runtime_error("failed to add conv2d op to resolver");
+    if (resolver.AddTranspose() != kTfLiteOk) throw std::runtime_error("failed to add transpose op to resolver");
+    if (resolver.AddLeakyRelu() != kTfLiteOk) throw std::runtime_error("failed to add leaky relu op to resolver");
+    if (resolver.AddFullyConnected() != kTfLiteOk) throw std::runtime_error("failed to add fully connected op to resolver");
+    if (resolver.AddDequantize() != kTfLiteOk) throw std::runtime_error("failed to add dequantize op to resolver");
 
-    tflite::MicroMutableOpResolver<6> resolver;
-    if (resolver.AddQuantize() != kTfLiteOk) throw std::runtime_error("failed to add op to resolver");
     tflite::MicroInterpreter interpreter { model, resolver, tensor_arena, tensor_arena_size };
-
-    if (interpreter.AllocateTensors() != kTfLiteOk) {
-        throw std::runtime_error("failed to allocate tensors");
-    }
+    if (interpreter.AllocateTensors() != kTfLiteOk) throw std::runtime_error("failed to allocate tensors");
 
     Tensor<f32, 1> res { nullptr, nullptr, 0 };
     return res;
