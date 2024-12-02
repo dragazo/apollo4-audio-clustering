aa2acbd3fa82ad28f5e150d41943d06a82cd1c3b
Devin Jean <emailcruzjean@yahoo.com>
2024-12-02 11:16:48 -0600

    tf glue

--- src/app/ai.cpp
+++ src/app/ai.cpp
@@ -1,11 +1,11 @@
 #include "../ai/util.h"
+#include "../ai/tf.h"
 
 extern "C" {
-    void preprocess(float *input, unsigned input_len, unsigned sample_rate, float **output, unsigned *output_rows, unsigned *output_cols, void (**output_deleter)(float*)) {
+    void preprocess_and_encode(float *input, unsigned input_len, float sample_rate, float *output) {
         Tensor<f32, 1> input_tensor { input, nullptr, input_len };
-        Tensor<f32, 2> res = mfcc_spectrogram_for_learning(input_tensor, (f32)sample_rate);
-        *output_rows = res.dim<0>();
-        *output_cols = res.dim<1>();
-        std::move(res).leak(*output, *output_deleter);
+        Tensor<f32, 2> prepped = mfcc_spectrogram_for_learning(input_tensor, (f32)sample_rate);
+        Tensor<f32, 1> res = inference(prepped);
+        for (u32 i = 0; i < res.dim<0>(); ++i) output[i] = res(i);
     }
 }
