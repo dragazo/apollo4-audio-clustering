e839aaefb3f5a4be50a2acc3d44b9eb1e9b5525d
Devin Jean <emailcruzjean@yahoo.com>
2025-04-17 17:21:53 -0500

    more caching

--- src/ai/tf.cpp
+++ src/ai/tf.cpp
@@ -12,6 +12,7 @@ Tensor<f32, 1> inference(const Tensor<f32, 2> &x) {
         const tflite::Model *model;
         tflite::MicroMutableOpResolver<7> resolver;
         char interpreter[sizeof(tflite::MicroInterpreter)];
+        TfLiteTensor *input, *output;
         Cache() {
             model = tflite::GetModel(model_tflite);
             if (model->version() != TFLITE_SCHEMA_VERSION) THROW(std::runtime_error("wrong model schema version"));
@@ -26,23 +27,24 @@ Tensor<f32, 1> inference(const Tensor<f32, 2> &x) {
 
             new (interpreter) tflite::MicroInterpreter { model, resolver, tensor_arena, tensor_arena_size };
             if (reinterpret_cast<tflite::MicroInterpreter*>(interpreter)->AllocateTensors() != kTfLiteOk) THROW(std::runtime_error("failed to allocate tensors"));
+
+            input = reinterpret_cast<tflite::MicroInterpreter*>(interpreter)->input(0);
+            output = reinterpret_cast<tflite::MicroInterpreter*>(interpreter)->output(0);
+
+            if (input->type != kTfLiteFloat32) THROW(std::runtime_error("model input is not f32"));
+            if (output->type != kTfLiteFloat32) THROW(std::runtime_error("model output is not f32"));
+
+            if (input->dims->size != 3 || input->dims->data[0] != 1) THROW(std::runtime_error("input wrong shape"));
+            if (output->dims->size != 2 || output->dims->data[0] != 1) THROW(std::runtime_error("output wrong shape"));
         }
     } cache;
 
-    tflite::MicroInterpreter &interpreter = *reinterpret_cast<tflite::MicroInterpreter*>(cache.interpreter);
+    if (cache.input->dims->data[1] != x.dim<0>() || cache.input->dims->data[2] != x.dim<1>()) THROW(std::runtime_error("input wrong shape"));
+    for (u32 i = x.size(); i-- > 0; ) cache.input->data.f[i] = (&x(0,0))[i];
 
-    TfLiteTensor *input = interpreter.input(0);
-    TfLiteTensor *output = interpreter.output(0);
+    if (reinterpret_cast<tflite::MicroInterpreter*>(cache.interpreter)->Invoke() != kTfLiteOk) THROW(std::runtime_error("failed to execute model"));
 
-    if (input->type != kTfLiteFloat32) THROW(std::runtime_error("model input is not f32"));
-    if (input->dims->size != 3 || input->dims->data[0] != 1 || input->dims->data[1] != x.dim<0>() || input->dims->data[2] != x.dim<1>()) THROW(std::runtime_error("input wrong shape"));
-    for (u32 i = x.size(); i-- > 0; ) input->data.f[i] = (&x(0,0))[i];
-
-    if (interpreter.Invoke() != kTfLiteOk) THROW(std::runtime_error("failed to execute model"));
-
-    if (output->type != kTfLiteFloat32) THROW(std::runtime_error("model output is not f32"));
-    if (output->dims->size != 2 || output->dims->data[0] != 1) THROW(std::runtime_error("output wrong shape"));
-    Tensor<f32, 1> res { new f32[output->dims->data[1]], [](auto *p) { delete[] p; }, output->dims->data[1] };
-    for (u32 i = 0; i < res.dim<0>(); ++i) res(i) = output->data.f[i];
+    Tensor<f32, 1> res { new f32[cache.output->dims->data[1]], [](auto *p) { delete[] p; }, cache.output->dims->data[1] };
+    for (u32 i = 0; i < res.dim<0>(); ++i) res(i) = cache.output->data.f[i];
     return res;
 }
